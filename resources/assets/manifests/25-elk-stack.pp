9f8c7f3c0837adb6040a6583a387c61c5fee691b
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-01 15:02:42 -0500

    Current work checkpoint. See diff for details.

--- resources/assets/manifests/25-elk-stack.pp
+++ resources/assets/manifests/25-elk-stack.pp
@@ -33,7 +33,7 @@ class elk( $root ) {
 
   exec { "install-elasticsearch":
     unless  => 'service elasticsearch status',
-    command => "wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && echo 'deb http: //packages.elastic.co/elasticsearch/2.x/debian stable main' | sudo tee -a /etc/apt/sources.list.d/elasticsearch.list && sudo apt-get -qq update && sudo apt-get -yq install elasticsearch",
+    command => "wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && echo 'deb http://packages.elastic.co/elasticsearch/2.x/debian stable main' | sudo tee -a /etc/apt/sources.list.d/elasticsearch.list && sudo apt-get -qq update && sudo apt-get -yq install elasticsearch",
     cwd     => $root,
   }
 
