bede59ed05082077e9d2daf5cbb1d1f8c68f1b10
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-01 14:35:27 -0500

    Current work checkpoint. See diff for details.

--- resources/assets/manifests/25-elk-stack.pp
+++ resources/assets/manifests/25-elk-stack.pp
@@ -2,59 +2,87 @@
 # DreamFactory Enterprise(tm) Installer Manifest
 # (c) 2012-∞ by DreamFactory Software, Inc. All Rights Reserved.
 #
-# Installs Elasticsearch, Logstash, and Kibana
+# Installs MySQL/Percona server
 ################################################################################
 
 notify { 'announce-thyself':
-  message => '[DFE] Install/update ELK stack',
+  message => '[DFE] The Mighty ELK',
 }
 
-class { "apt":
-  update => {
-    frequency => daily
+class bootstrap {
+
+  File { owner => 0, group => 0, mode => 0644 }
+
+  # ensure local apt cache index is up to date before beginning
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
   }
+
 }
 
-apt::source { "elasticsearch.debian":
-  comment  => "Repo for elasticsearch",
-  location => "http://packages.elastic.co/elasticsearch/2.x",
-  release  => "debian",
-  repos    => "main",
-  key      => {
-    "id"     => "46095ACC8548582C1A2699A9D27D666CD88E42B4",
-    "server" => "keys.gnupg.net"
-  },
-  include  => {
-    "src" => false,
-    "deb" => true
-  },
-  notify   => Exec["apt-update"]
-}
-
-##  Elasticsearch
-file { '/opt/elasticsearch':
-  ensure => directory,
-  owner  => $www_user,
-  group  => $group,
-} ->
-class { "elasticsearch":
-  restart          => true,
-  require          => Apt::Source["elasticsearch.debian"],
-  java_install     => true,
-  config           => {
-    'cluster' => {
-      'name' => "cluster-${vendor_id}",
-    },
-    'index'   => {
-      'number_of_replicas' => '0',
-      'number_of_shards'   => '1',
-    },
-    'network' => {
-      'host' => '0.0.0.0',
-    }
+##  ELK stack installer
+class elk {
+  file { ["/opt/sites", "/opt/sites/kibana", "/opt/sites/test"]:
+    ensure  => directory,
+    owner   => $www_user,
+    group   => $group,
+    mode    => 2755,
+    recurse => true,
   }
-} ->
-service { "elasticsearch-service":
-  name   => "elasticsearch",
-  ensure => running
+
+  exec { "install-java8":
+    command => "add-apt-repository -y ppa:webupd8team/java && sudo apt-get update && echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections && sudo apt-get -y install oracle-java8-installer",
+    require => Class["bootstrap"]
+  }
+
+  exec { "install-elasticsearch":
+    unless  => 'service elasticsearch status',
+    command => "wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && echo 'deb http: //packages.elastic.co/elasticsearch/2.x/debian stable main' | sudo tee -a /etc/apt/sources.list.d/elasticsearch.list && sudo apt-get -qq update && sudo apt-get -yq install elasticsearch",
+  }
+
+  # custom configuration
+  file{ '/etc/elasticsearch/elasticsearch.yml' :
+    ensure  => present,
+    source  => "$PWD/resources/assets/etc/elasticsearch.yml",
+    require => Exec["install-elasticsearch"]
+  }
+
+  # restart elasticsearch service
+  service { "elasticsearch":
+    ensure  => running,
+    require => [Exec['install-elasticsearch'], File['/etc/elasticsearch/elasticsearch.yml']],
+  }
+
+  exec { "install kopf":
+    cwd     => "/home/$user",
+    command => "sudo /usr/share/elasticsearch/bin/plugin -install lmenezes/elasticsearch-kopf",
+    require => Exec['install-elasticsearch']
+  }
+
+  exec { "download-kibana4":
+    cwd     => "/opt/sites/kibana",
+    command => "wget https://download.elastic.co/kibana/kibana/kibana-4.2.0-linux-x64.tar.gz",
+    creates => "/opt/sites/kibana/kibana-4.2.0-linux-x64.tar.gz",
+  }
+
+  exec { "install-kibana4":
+    # unless => 'service logstash status',
+    cwd     => "/opt/sites/kibana",
+    command => "tar xvfz kibana-4.2.0-linux-x64.tar.gz && mv /opt/sites/kibana/kibana*64/* /opt/sites/kibana",
+    require => Exec["download-kibana4"],
+  }
+
+  exec { "download-logstash":
+    cwd     => "/var/cache/apt/archives",
+    command => "wget https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.0.0-1_all.deb",
+    creates => "/var/cache/apt/archives/logstash_2.0.0-1_all.deb",
+  }
+
+  exec { "install-logstash":
+    unless  => 'service logstash status',
+    cwd     => "/var/cache/apt/archives",
+    command => "dpkg -i logstash_2.0.0-1_all.deb && update-rc.d logstash defaults 95 10 && /etc/init.d/logstash restart",
+    require => Exec["download-logstash"]
+  }
+
 }
