397669d9a51b077425c31e44803aae47ce91da80
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-03 17:28:04 -0500

    more elk options

--- app/Installer.php
+++ app/Installer.php
@@ -149,12 +149,12 @@ class Installer
                     break;
 
                 case 'dc_install_elk':
-                    $_value = !empty($_value);
+                    $_value = (1 == $_value ? 'true' : 'false');
                     break;
             }
 
             //  Dump non-empties into the source file
-            if (!empty($_value)) {
+            if (!empty($_value) && $_cleanKey == 'dc_install_elk') {
                 $_facterData['export FACTER_' . strtoupper($_cleanKey)] = $_value;
             }
 
