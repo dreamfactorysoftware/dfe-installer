2b72fa8610ce385296817fcbe1e3d1fe6aeec46f
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-03 17:00:19 -0500

    Current work checkpoint. See diff for details.

--- resources/assets/manifests/25-elk-stack.pp
+++ resources/assets/manifests/25-elk-stack.pp
@@ -5,18 +5,9 @@
 # Installs MySQL/Percona server
 ################################################################################
 
-notify { 'announce-thyself':
-  message => '[DFE] The Mighty ELK',
-}
-
-File { owner => 0, group => 0, mode => 0644, }
-
-Exec { path => ['/usr/bin','/usr/sbin','/bin','/sbin'], }
-
-# ensure local apt cache index is up to date before beginning
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
-}
+##------------------------------------------------------------------------------
+## Variables
+##------------------------------------------------------------------------------
 
 $_logstashConfig = "input {
   gelf {
@@ -46,9 +37,32 @@ output {
 }
 "
 
+##------------------------------------------------------------------------------
+## Defaults
+##------------------------------------------------------------------------------
+
+notify { 'announce-thyself':
+  message => '[DFE] The Mighty ELK',
+}
+
+File { owner => 0, group => 0, mode => 0644, }
+
+Exec { path => ['/usr/bin','/usr/sbin','/bin','/sbin'], }
+
+# ensure local apt cache index is up to date before beginning
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
+}
+
 ##  ELK stack installer
 class elk( $root ) {
-  file { ["/opt/sites", "/opt/sites/test", "/opt/sites/_releases", "/opt/sites/_releases/kibana"]:
+
+  file { [
+    $root,
+    "$_installRoot/_releases",
+    "$root/_releases/kibana",
+    "$root/_releases/logstash",
+  ]:
     ensure  => directory,
     owner   => $www_user,
     group   => $group,
@@ -56,11 +70,13 @@ class elk( $root ) {
     recurse => true,
   }
 
+  ##  Java
   exec { "install-java8":
     command => "add-apt-repository -y ppa:webupd8team/java && sudo apt-get update && echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections && sudo apt-get -y install oracle-java8-installer",
     cwd     => $root,
   }
 
+  ##  Elasticsearch
   exec { "install-elasticsearch":
     unless  => 'service elasticsearch status',
     command => "wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - && echo 'deb http://packages.elastic.co/elasticsearch/2.x/debian stable main' | sudo tee -a /etc/apt/sources.list.d/elasticsearch.list && sudo apt-get -qq update && sudo apt-get -yq install elasticsearch",
@@ -72,50 +88,64 @@ class elk( $root ) {
     ensure  => running,
     require => [
       Exec['install-elasticsearch'],
-      #File['/etc/elasticsearch/elasticsearch.yml'],
     ],
   }
 
+  ##  Kibana
   exec { "download-kibana4":
-    cwd     => "/opt/sites/_releases/kibana",
+    cwd     => "$root/_releases/kibana",
     command => "wget https://download.elastic.co/kibana/kibana/kibana-4.2.0-linux-x64.tar.gz",
-    creates => "/opt/sites/_releases/kibana/kibana-4.2.0-linux-x64.tar.gz",
+    creates => "$root/_releases/kibana/kibana-4.2.0-linux-x64.tar.gz",
   }
 
   exec { "install-kibana4":
     # unless => 'service logstash status',
-    cwd     => "/opt/sites/_releases/kibana",
+    cwd     => "$root/_releases/kibana",
     user    => $www_user,
     group   => $group,
     command => "tar xzf kibana-4.2.0-linux-x64.tar.gz",
     require => Exec["download-kibana4"],
   }->
-  file { "/opt/sites/kibana":
+  file { "$root/kibana":
     ensure => link,
-    target => "/opt/sites/_releases/kibana/kibana-4.2.0-linux-x64",
+    target => "$root/_releases/kibana/kibana-4.2.0-linux-x64",
+  }
+
+  ##  Logstash
+  exec { "download-logstash":
+    cwd     => "$root/_releases/logstash",
+    command => "wget https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.0.0-1_all.deb",
+    creates => "$root/_releases/logstash/logstash_2.0.0-1_all.deb",
   }
 
   exec { "install-logstash":
+    # unless => 'service logstash status',
+    cwd     => "$root/_releases/logstash",
+    user    => $www_user,
+    group   => $group,
+    command => "dpkg -i logstash_2.0.0-1_all.deb",
+    require => Exec['download-logstash'],
     unless  => 'service logstash status',
-    command => "echo 'deb http://packages.elastic.co/logstash/2.0/debian stable main' | sudo tee -a /etc/apt/sources.list.d/logstash.list && sudo apt-get -qq update && sudo apt-get -yq install logstash",
-    cwd     => $root,
-  }->
-    ##  Cluster configuration
+  }
+
+  # restart logstash service
+  service { "logstash":
+    ensure  => running,
+    require => [
+      Exec['install-logstash'],
+    ],
+  }
+
+  ##  Cluster configuration
   file { '/etc/logstash/conf.d/100-dfe-cluster.conf':
     ensure  => file,
     content => $_logstashConfig,
-  }
-  
-  exec { "restart-logstash":
-    onlyif  => 'service logstash status',
-    command => "service logstash restart",
-    cwd     => $root,
     require => Exec['install-logstash'],
+    notify  => Service['logstash'],
   }
-
 
 }
 
 class { elk:
-  root => $pwd,
+  root => '/opt/elk',
 }
