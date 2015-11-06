368fd15cbea7377096a72a0befa89ca2f81ebde0
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-06 13:09:09 -0500

    Current work checkpoint. See diff for details.

--- resources/assets/manifests/00-user.pp
+++ resources/assets/manifests/00-user.pp
@@ -37,7 +37,7 @@ class updateHostsFile( $hostname, $ip = '127.0.1.1' ) {
 }
 
 ##  Create the DFE admin user
-class createAdminUser( $root ) {
+class createAdminUser( $root, $token ) {
   ## Only new installations get a user
   if ( false == str2bool($dfe_update) ) {
     user { $user:
@@ -59,6 +59,18 @@ alias lvcc='sudo rm -rf /tmp/.df-cache/ /var/www/console/storage/bootstrap/cache
 alias ngtr='sudo service php5-fpm stop ; sudo service nginx stop ; sudo service php5-fpm start ; sudo service nginx start'
 "
     }->
+    file { "$root/.composer":
+      ensure => directory,
+      owner  => $user,
+      group  => $group,
+    }->
+    file { "$root/.composer/auth.json":
+      ensure  => file,
+      owner   => $user,
+      group   => $group,
+      mode    => '640',
+      content => "{\"github-oauth\": {\"github.com\": \"$token\"}}",
+    }->
     file { "$root/.ssh":
       ensure  => directory,
       owner   => $user,
@@ -75,32 +87,13 @@ alias ngtr='sudo service php5-fpm stop ; sudo service nginx stop ; sudo service 
     file_line { 'add-user-to-sudoers':
       path    => '/etc/sudoers',
       line    => "$user  ALL=(ALL) NOPASSWD:ALL",
+    }->
+    file { "$root/.gitconfig":
+      ensure => present,
+      owner  => $user,
+      group  => $group,
+      source => "$pwd/resources/assets/git/gitconfig",
     }
-  }
-}
-
-## Create and seed $root/.gitconfig and git auth for composer
-class configureGitAuth( $root, $token ) {
-  file { [
-    $root,
-    "$root/.composer",
-  ]:
-    ensure => directory,
-    owner  => $user,
-    group  => $group,
-  }->
-  file { "$root/.gitconfig":
-    ensure => present,
-    owner  => $user,
-    group  => $group,
-    source => "$pwd/resources/assets/git/gitconfig",
-  }->
-  file { "$root/.composer/auth.json":
-    ensure  => file,
-    owner   => $user,
-    group   => $group,
-    mode    => '640',
-    content => "{\"github-oauth\": {\"github.com\": \"$token\"}}",
   }
 }
 
@@ -123,9 +116,6 @@ class { "updateHostsFile":
 
 ## Create user and git auth
 class { "createAdminUser":
-  root  => "/home/$user",
-}->
-class { "configureGitAuth":
   root    => "/home/$user",
   token   => $gh_token,
 }
