8bbb67e422a79cdae11ca793d4ad54e874f42d01
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-20 15:18:21 -0500

    fixed bool string

--- resources/assets/manifests/30-packages.pp
+++ resources/assets/manifests/30-packages.pp
@@ -70,8 +70,8 @@ class { updatePackages:
   stage => 'pre',
 }->
 class { postfix:
-  service_enable => 'true',
-  service_ensure => 'running',
+  service_enable => true,
+  service_ensure => running,
 }
 
 exec { 'enable-mcrypt-settings':
