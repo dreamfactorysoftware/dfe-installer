57f0b567470e6a0a6f508966f672c00dac07e78f
Jerry Ablan <jerryablan@dreamfactory.com>
2016-02-03 19:32:24 -0500

    release/1.1.55 prep

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# @(#)$Id: update.sh,v 1.1.54 2015-11-16 dweiner/jablan $
+# @(#)$Id: update.sh,v 1.1.55 2015-11-16 dweiner/jablan $
 #
 # This file is part of DreamFactory Enterprise(tm)
 #
